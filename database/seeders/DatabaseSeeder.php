68057894188a0ed00ad5558a91fe25f0c4240942
Darma Riduan <dharmarhiduwhan@gmail.com>
2024-07-26 09:45:11 +0700

    category frontend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Darma Riduan',
+            'email' => 'admin@freehost.id',
+            'password' => Hash::make('admin@freehost.id'),
+            'email_verified_at' => now(),
         ]);
     }
 }
